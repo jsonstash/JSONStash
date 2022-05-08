7fef7a2ae9451943110a076bcfd6ec44cfc52bb1
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-05-07 20:35:06 -0500

    Separated out stash data with it's metadata in preparation for stash quota

--- src/JSONStash.Common/Models/StashData.cs
+++ src/JSONStash.Common/Models/StashData.cs
@@ -2,26 +2,14 @@
 {
     public class StashData
     {
-        public string Name { get; set; }
-
         public string Data { get; set; }
 
-        public DateTimeOffset Created { get; set; }
-
-        public DateTimeOffset? Modified { get; set; }
-
-        public Guid StashId { get; set; }
-
-        public Guid CollectionId { get; set; }
+        public StashMetadata Metadata { get; set; }
 
         public StashData(Stash stash)
         {
-            Name = stash.Name;
             Data = stash.Data;
-            Created = stash.Created;
-            Modified = stash.Modified;
-            StashId = stash.StashGuid;
-            CollectionId = stash.Collection.CollectionGuid;
+            Metadata = new(stash);
         }
     }
 }
---
+++ src/JSONStash.Common/Models/StashMetadata.cs
@@ -0,0 +1,24 @@
+﻿namespace JSONStash.Common.Models
+{
+    public class StashMetadata
+    {
+        public string Name { get; set; }
+
+        public DateTimeOffset Created { get; set; }
+
+        public DateTimeOffset? Modified { get; set; }
+
+        public Guid StashId { get; set; }
+
+        public Guid CollectionId { get; set; }
+
+        public StashMetadata(Stash stash)
+        {
+            Name = stash.Name;
+            Created = stash.Created;
+            Modified = stash.Modified;
+            StashId = stash.StashGuid;
+            CollectionId = stash.Collection.CollectionGuid;
+        }
+    }
+}
